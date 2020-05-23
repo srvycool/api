8366d0e9395b7216b371b9249eceabb7535bf7cf
Henrik Fricke <henrikfricke@web.de>
2020-05-23 21:47:57 +0200

    GitHub workflow (#9)
    * feat: e2e tests
    
    * feat: init github workflow
    
    * refactor: run prod deployment after staging
    
    * fix: typo in imports

--- bin/serverless-survey.ts
+++ bin/serverless-survey.ts
@@ -3,8 +3,14 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { ServerlessSurveyStack } from "../lib/serverless-survey-stack";
 
+const environment = process.env.ENVIRONMENT;
+
+if (!environment) {
+  throw new Error("Environment is missing!");
+}
+
 const app = new cdk.App();
-new ServerlessSurveyStack(app, "ServerlessSurveyStack", {
+new ServerlessSurveyStack(app, `ServerlessSurveyStack (${environment})`, {
   env: {
     region: "eu-west-1",
   },
--- templates/index.ts
+++ templates/index.ts
@@ -5,6 +5,6 @@ export * as querySurvey from "./querySurvey";
 export * as mutationSurveyMultipleChoiceCreate from "./mutationSurveyMultipleChoiceCreate";
 export * as mutationSurveyMultipleChoiceSubmit from "./mutationSurveyMultipleChoiceSubmit";
 export * as mutationSurveyTextareaSubmit from "./mutationSurveyTextareaSubmit";
-export * as mutationSurveyTextareaCreate from "./mutationsurveyTextareaCreate";
+export * as mutationSurveyTextareaCreate from "./mutationSurveyTextareaCreate";
 export * as surveyMultipleChoiceAnswers from "./surveyMultipleChoiceAnswers";
 export * as surveyTextareaSubmission from "./surveyTextareaSubmissions";
